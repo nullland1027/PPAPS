19da722d92db70c0797440f91e0d920ab16fc0d0
nullland1027 <nullland1027@outlook.com>
2023-01-28 22:31:43 +0800

    Remove some bugs

--- algorithm/main.py
+++ algorithm/main.py
@@ -3,6 +3,7 @@ import numpy as np
 from datasets import PlantDataSet
 from datasets import AnimalDataSet
 from networks import RFPredictor
+from networks import XGBoostPredictor
 import pandas as pd
 from datasets import DataSet
 from sklearn import preprocessing
@@ -31,29 +32,76 @@ def data_process(path, kind='plant'):
     np.save(os.path.join(target_path, kind + '_label.npy'), dataset_obj.get_label())
 
 
-def rf_adjust_params():
+def rf_adjust_params(rf_predictor):
+    """
+
+    @param rf_predictor:
+    @return:
+    """
     search = {
         'n_estimators': list(range(100, 400, 50)),
         'min_samples_split': list(range(8, 20)),
         'min_samples_leaf': list(range(1, 5)),
         'min_weight_fraction_leaf': [0.3, 0.4, 0.5]
     }
-    print(rf.search_params(search))
+    print(rf_predictor.search_params(search))  # Predictor will update inner RF model automatically
+    return rf_predictor
 
 
 if __name__ == '__main__':
     animal_data = os.path.join('raw_data', 'animal', 'animal.npy')
     animal_label = os.path.join('raw_data', 'animal', 'animal_label.npy')
-    # plant = os.path.join('raw_data', 'plant', 'plant_all_data.csv')
+    plant_data = os.path.join('raw_data', 'plant', 'plant.npy')
+    plant_label = os.path.join('raw_data', 'plant', 'plant_label.npy')
 
-    rf_params_dft = {  # default params
+    # =========Random Forest ==============
+    rf_params_bst = {  # best params
         'n_estimators': 250,
         'min_samples_split': 17,
         'min_samples_leaf': 4,
         'min_weight_fraction_leaf': 0.3,
         'n_jobs': -1
     }
-    rf = RFPredictor('animal', np.load(animal_data), np.load(animal_label), **rf_params_dft)
-    # rf.train()
+    rf = RFPredictor('animal', np.load(animal_data), np.load(animal_label), **rf_params_bst)
+    rf = rf_adjust_params(rf)
+    rf.train()
+    rf.save_model('models')
 
-    rf.save_model('random_forest_animal', os.path.join('models'))
+    # ========XGBoost ===================
+    xgb_params = {
+        'n_estimators': 400,
+        'max_depth': 3,
+        'learning_rate': 0.1,
+
+        'min_child_weight': 5,
+        'gamma': 5,
+        'subsample': 0.8,
+        'colsample_bytree': 0.6,
+        'scale_pos_weight': 1,
+
+        'objective': 'binary:logistic',  # loss function
+        'eval_metric': 'auc',
+        'n_jobs': -1,
+        'nthread': -1,
+        'random_state': 42
+    }
+
+    xgb_search = {
+        'max_depth': [4, 6, 8, 10],
+        'n_estimators': [350, 400, 450],
+        'min_child_weight': [4, 5, 6, 7],
+        'gamma': [2, 5, 10],
+        'subsample': [0.2, 0.5, 0.8],
+        'colsample_bytree': [0.5, 0.6, 0.8],
+        'scale_pos_weight': [0.2, 0.5, 0.9],
+    }
+    ani_xgb = XGBoostPredictor('animal', np.load(animal_data), np.load(animal_label), **xgb_params)
+    print(ani_xgb.search_params(xgb_search))  # output best score and params
+    ani_xgb.train()
+    ani_xgb.save_model('/models')
+
+    # ani_xgb.load_model(os.path.join('models', 'xgboost_animal.pickle'))
+    # ani_xgb.predict(os.path.join('raw_data', 'animal', 'Blind_Animal.csv'))
+    # print(ani_xgb.xgb)
+    # ani_xgb.output_metrix()
+    # ani_xgb.show_ROC_curve()
--- algorithm/networks.py
+++ algorithm/networks.py
@@ -2,10 +2,12 @@ import abc
 import os.path
 from tqdm import tqdm
 import pickle
-import xgboost
+from matplotlib import pyplot as plt
 import numpy as np
+from xgboost import XGBClassifier
 from abc import ABC, abstractmethod
 from lightgbm import LGBMClassifier
+from sklearn import metrics
 from datasets import AnimalDataSet, PlantDataSet
 from sklearn.model_selection import GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
@@ -25,7 +27,7 @@ class Predictor(ABC):
         pass
 
     @abstractmethod
-    def save_model(self, name, path):
+    def save_model(self, path):
         pass
 
     @abstractmethod
@@ -84,18 +86,27 @@ class RFPredictor(Predictor):
 
     def search_params(self, params_dict: dict):
         adapter = GridSearchCV(estimator=self.rf_classifier,
-                               scoring='f1_macro',
+                               scoring='f1',
                                param_grid=params_dict,
                                verbose=10)  # using grid search to find the best params
         adapter.fit(self._X, self._y)
         self.rf_classifier = adapter.best_estimator_  # Update the model
-        return adapter.best_score_, adapter.best_estimator_
+        return adapter.best_score_, adapter.best_params_
 
-    def save_model(self, name: str, path: str):
-        pickle.dump(self.rf_classifier, open(os.path.join(path, name) + '.pickle', 'wb'))
+    def save_model(self, path: str):
+        """
+        Save a model in the given path(folder)
+        @param path: given folder
+        @return: None
+        """
+        file_name = os.path.join(path, self._kind) + '.pickle'
+        if os.path.exists(file_name):
+            os.remove(file_name)
+        pickle.dump(self.rf_classifier, open(file_name, 'wb'))
 
     def load_model(self, file_name):
-        return pickle.load(open(file_name, 'rb'))
+        self.rf_classifier = pickle.load(open(file_name, 'rb'))
+        return self.rf_classifier
 
     def feature_importance(self):
         return self.rf_classifier.feature_importances_
@@ -124,30 +135,80 @@ class XGBoostPredictor(Predictor):
         self._y = y
         self.params: dict = kwargs  # learner's params
         self._k_fold = StratifiedKFold(n_splits=5, random_state=42, shuffle=True)  # Cross validation
-        self.xgb = xgboost.XGBClassifier(**self.params)  # Build model
+        self.xgb = XGBClassifier(**self.params)  # Build model
         self.blind_y_ture = None
         self.blind_y_pred = None
 
     def train(self):
-        pass
+        for train_idx, test_idx in self._k_fold.split(self._X, self._y):
+            X_train, y_train = self._X[train_idx, :], self._y[train_idx]
+            X_test, y_test = self._X[test_idx, :], self._y[test_idx]
 
-    def predict(self, target_file):
-        pass
-
-    def feature_importance(self):
-        pass
-
-    def load_model(self, file_name):
-        pass
-
-    def output_metrix(self):
-        pass
+            self.xgb.fit(X_train, y_train)
+            print('Accuracy:', self.xgb.score(X_test, y_test))  # Show the accuracy of each epoch
 
     def search_params(self, params_dict):
-        pass
+        adapter = GridSearchCV(estimator=self.xgb,
+                               scoring='roc_auc',
+                               param_grid=params_dict,
+                               cv=5,
+                               verbose=10)  # using grid search to find the best params
+        adapter.fit(self._X, self._y)
+        self.xgb = adapter.best_estimator_  # Update the model=======THE MOST IMPORTANT CODE
+        return adapter.best_score_, adapter.best_params_
 
-    def save_model(self, name, path):
-        pass
+    def feature_importance(self):
+        return self.xgb.feature_importances_
+
+    def save_model(self, path):
+        """
+        Save the XGBoost model
+        @param path: The folder name NOT FILE NAME!!!!!!
+        @return: None
+        """
+        try:
+            file_name = os.path.join(path, 'xgboost_' + self._kind + '.pickle')
+            if os.path.exists(file_name):
+                os.remove(file_name)
+            pickle.dump(self.xgb, open(file_name, 'wb'))
+            print('MODEL SAVED.')
+        except FileExistsError as fee:
+            print('File exists ERROR!')
+        except FileNotFoundError as fnfe:
+            print('File not found ERROR!')
+
+    def load_model(self, file_name):
+        self.xgb = pickle.load(open(file_name, 'rb'))
+
+    def predict(self, target_file):
+        dataset_obj = None
+        if self._kind == 'plant':
+            dataset_obj = PlantDataSet(target_file)
+        elif self._kind == 'animal':
+            dataset_obj = AnimalDataSet(target_file)
+        else:
+            raise Exception("No this item.")
+        dataset_obj.data_clean()
+        dataset_obj.normalize('l2')
+        self.blind_y_ture = dataset_obj.get_label()
+        self.blind_y_pred = self.xgb.predict(dataset_obj.get_data())
+
+    def output_metrix(self):
+        Predictor._report(self.blind_y_ture, self.blind_y_pred)
+
+    def show_ROC_curve(self):
+        fpr, tpr, threshold = metrics.roc_curve(self.blind_y_ture, self.blind_y_pred)
+        roc_auc = metrics.auc(fpr, tpr)
+        plt.figure(figsize=(6, 6))
+        plt.title('Validation ROC')
+        plt.plot(fpr, tpr, 'b', label='Val AUC = %0.3f' % roc_auc)
+        plt.legend(loc='lower right')
+        plt.plot([0, 1], [0, 1], 'r--')
+        plt.xlim([0, 1])
+        plt.ylim([0, 1])
+        plt.ylabel('True Positive Rate')
+        plt.xlabel('False Positive Rate')
+        plt.show()
 
 
 class LGBMPredictor(Predictor):
