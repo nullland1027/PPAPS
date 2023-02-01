7b07b8e47531e2588aaf5ac07fef475c74ba7780
nullland1027 <nullland1027@outlook.com>
2023-02-01 11:04:20 +0800

    Finished to train the animal datasets

--- algorithm/main.py
+++ algorithm/main.py
@@ -4,10 +4,9 @@ from datasets import PlantDataSet
 from datasets import AnimalDataSet
 from networks import RFPredictor
 from networks import XGBoostPredictor
-import pandas as pd
-from datasets import DataSet
-from sklearn import preprocessing
-from sklearn import model_selection
+from networks import LGBMPredictor
+
+import time
 
 
 def data_process(path, kind='plant'):
@@ -48,45 +47,13 @@ def rf_adjust_params(rf_predictor):
     return rf_predictor
 
 
-if __name__ == '__main__':
-    animal_data = os.path.join('raw_data', 'animal', 'animal.npy')
-    animal_label = os.path.join('raw_data', 'animal', 'animal_label.npy')
-    plant_data = os.path.join('raw_data', 'plant', 'plant.npy')
-    plant_label = os.path.join('raw_data', 'plant', 'plant_label.npy')
+def xgboost_adjust_params(xgboost_predictor):
+    """
 
-    # =========Random Forest ==============
-    rf_params_bst = {  # best params
-        'n_estimators': 250,
-        'min_samples_split': 17,
-        'min_samples_leaf': 4,
-        'min_weight_fraction_leaf': 0.3,
-        'n_jobs': -1
-    }
-    rf = RFPredictor('animal', np.load(animal_data), np.load(animal_label), **rf_params_bst)
-    rf = rf_adjust_params(rf)
-    rf.train()
-    rf.save_model('models')
-
-    # ========XGBoost ===================
-    xgb_params = {
-        'n_estimators': 400,
-        'max_depth': 3,
-        'learning_rate': 0.1,
-
-        'min_child_weight': 5,
-        'gamma': 5,
-        'subsample': 0.8,
-        'colsample_bytree': 0.6,
-        'scale_pos_weight': 1,
-
-        'objective': 'binary:logistic',  # loss function
-        'eval_metric': 'auc',
-        'n_jobs': -1,
-        'nthread': -1,
-        'random_state': 42
-    }
-
-    xgb_search = {
+    @param xgboost_predictor:
+    @return:
+    """
+    search = {
         'max_depth': [4, 6, 8, 10],
         'n_estimators': [350, 400, 450],
         'min_child_weight': [4, 5, 6, 7],
@@ -95,13 +62,105 @@ if __name__ == '__main__':
         'colsample_bytree': [0.5, 0.6, 0.8],
         'scale_pos_weight': [0.2, 0.5, 0.9],
     }
-    ani_xgb = XGBoostPredictor('animal', np.load(animal_data), np.load(animal_label), **xgb_params)
-    print(ani_xgb.search_params(xgb_search))  # output best score and params
-    ani_xgb.train()
-    ani_xgb.save_model('/models')
+    print(xgboost_predictor.search_params(search))
+    return xgboost_predictor
+
+
+def lgbm_adjust_params(lgb_predictor):
+    """
+    Output the best params
+    @param lgb_predictor:
+    @return:
+    """
+    search = {
+        'num_leaves': [10, 20, 31, 40],
+        'max_depth': [3, 5, 8, 10],
+        'learning_rate': [0.1, 0.05],
+        'n_estimators': list(range(100, 500, 100)),
+        'subsample_for_bin': [100000, 200000, 500000],
+        'min_split_gain': [0, 0.1, 0.5, 0.8],
+        'min_child_weight': [0.001, 0.005, 0.1],
+        'min_child_samples': [5, 10, 20, 50],
+        'colsample_bytree': [0.2, 0.5, 0.8],
+    }
+    print(lgb_predictor.search_params(search))
+    return lgb_predictor
+
+
+if __name__ == '__main__':
+    animal_data = os.path.join('raw_data', 'animal', 'animal.npy')
+    animal_label = os.path.join('raw_data', 'animal', 'animal_label.npy')
+    plant_data = os.path.join('raw_data', 'plant', 'plant.npy')
+    plant_label = os.path.join('raw_data', 'plant', 'plant_label.npy')
+
+    # =========Random Forest ==============
+    # rf_params_bst = {  # best params
+    #     'n_estimators': 200,
+    #     'min_samples_split': 8,
+    #     'min_samples_leaf': 3,
+    #     'min_weight_fraction_leaf': 0.4,
+    #     'n_jobs': -1
+    # }
+    # rf = RFPredictor('animal', np.load(animal_data), np.load(animal_label), **rf_params_bst)
+    # rf.load_model(os.path.join('models', 'random_forest_animal.pickle'))
+    # rf.predict(os.path.join('raw_data', 'animal', 'Blind_Animal.csv'))
+    # rf = rf_adjust_params(rf)
+    # rf.train()
+
+    # rf.output_metrix()
+
+    # ========XGBoost ===================
+    # xgb_params = {
+    #     'n_estimators': 350,
+    #     'max_depth': 4,
+    #     'learning_rate': 0.1,
+    #
+    #     'min_child_weight': 7,
+    #     'gamma': 10,
+    #     'subsample': 0.8,
+    #     'colsample_bytree': 0.5,
+    #     'scale_pos_weight': 0.5,
+    #
+    #     'objective': 'binary:logistic',  # loss function
+    #     'eval_metric': 'auc',
+    #     'n_jobs': -1,
+    #     'nthread': -1,
+    #     'random_state': 42
+    # }
+    #
+    # ani_xgb = XGBoostPredictor('animal', np.load(animal_data), np.load(animal_label), **xgb_params)
+    # print(ani_xgb.search_params(xgb_search))  # output best score and params
+    # ani_xgb.train()
+    # ani_xgb.save_model('models')
 
     # ani_xgb.load_model(os.path.join('models', 'xgboost_animal.pickle'))
     # ani_xgb.predict(os.path.join('raw_data', 'animal', 'Blind_Animal.csv'))
-    # print(ani_xgb.xgb)
     # ani_xgb.output_metrix()
     # ani_xgb.show_ROC_curve()
+
+    # ============LGBM===============
+
+    lgbm_params = {
+        'num_leaves': 10,
+        'max_depth': 3,
+        'learning_rate': 0.05,
+        'n_estimators': 100,
+        'subsample_for_bin': 100000,
+        'min_split_gain': 0.5,
+        'min_child_weight': 0.001,
+        'min_child_samples': 50,
+        'colsample_bytree': 0.5,
+
+        'objective': 'binary',
+        'metric': 'auc',  # 优化指标
+        'n_jobs': -1,
+        'force_col_wise': True,
+        'random_state': 42
+    }
+    lgbm_predictor = LGBMPredictor('animal', np.load(animal_data), np.load(animal_label), **lgbm_params)
+    start_time = time.time()
+    lgbm_predictor = lgbm_adjust_params(lgbm_predictor)
+    end_time = time.time()
+    print('Params searching costs', round((end_time - start_time) / 3600, 3), 'Hours')
+    lgbm_predictor.train()
+    lgbm_predictor.save_model('models')
--- algorithm/networks.py
+++ algorithm/networks.py
@@ -6,6 +6,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 from xgboost import XGBClassifier
 from abc import ABC, abstractmethod
+import lightgbm as lgb
 from lightgbm import LGBMClassifier
 from sklearn import metrics
 from datasets import AnimalDataSet, PlantDataSet
@@ -64,6 +65,27 @@ class Predictor(ABC):
         target_names = ['Class-0', 'Class-1']
         print(classification_report(y_true, y_pred, target_names=target_names))
 
+    @classmethod
+    def _show_ROC_curve(cls, blind_y_ture, blind_y_pred):
+        """
+        After the prediction to blind test data and Run to see the performance.
+        @param blind_y_ture: true val
+        @param blind_y_pred: prediction val
+        @return: None
+        """
+        fpr, tpr, threshold = metrics.roc_curve(blind_y_ture, blind_y_pred)
+        roc_auc = metrics.auc(fpr, tpr)
+        plt.figure(figsize=(6, 6))
+        plt.title('Validation ROC')
+        plt.plot(fpr, tpr, 'b', label='Val AUC = %0.3f' % roc_auc)
+        plt.legend(loc='lower right')
+        plt.plot([0, 1], [0, 1], 'r--')
+        plt.xlim([0, 1])
+        plt.ylim([0, 1])
+        plt.ylabel('True Positive Rate')
+        plt.xlabel('False Positive Rate')
+        plt.show()
+
 
 class RFPredictor(Predictor):
     def __init__(self, kind, X, y, **kwargs):
@@ -99,10 +121,16 @@ class RFPredictor(Predictor):
         @param path: given folder
         @return: None
         """
-        file_name = os.path.join(path, self._kind) + '.pickle'
-        if os.path.exists(file_name):
-            os.remove(file_name)
-        pickle.dump(self.rf_classifier, open(file_name, 'wb'))
+        try:
+            file_name = os.path.join(path, 'random_forest_' + self._kind) + '.pickle'
+            if os.path.exists(file_name):
+                os.remove(file_name)
+            pickle.dump(self.rf_classifier, open(file_name, 'wb'))
+            print('SAVED.')
+        except FileExistsError as fee:
+            print(fee)
+        except FileNotFoundError as fnfe:
+            print(fnfe)
 
     def load_model(self, file_name):
         self.rf_classifier = pickle.load(open(file_name, 'rb'))
@@ -173,9 +201,9 @@ class XGBoostPredictor(Predictor):
             pickle.dump(self.xgb, open(file_name, 'wb'))
             print('MODEL SAVED.')
         except FileExistsError as fee:
-            print('File exists ERROR!')
+            print('File exists ERROR!', fee)
         except FileNotFoundError as fnfe:
-            print('File not found ERROR!')
+            print('File not found ERROR!', fnfe)
 
     def load_model(self, file_name):
         self.xgb = pickle.load(open(file_name, 'rb'))
@@ -197,38 +225,81 @@ class XGBoostPredictor(Predictor):
         Predictor._report(self.blind_y_ture, self.blind_y_pred)
 
     def show_ROC_curve(self):
-        fpr, tpr, threshold = metrics.roc_curve(self.blind_y_ture, self.blind_y_pred)
-        roc_auc = metrics.auc(fpr, tpr)
-        plt.figure(figsize=(6, 6))
-        plt.title('Validation ROC')
-        plt.plot(fpr, tpr, 'b', label='Val AUC = %0.3f' % roc_auc)
-        plt.legend(loc='lower right')
-        plt.plot([0, 1], [0, 1], 'r--')
-        plt.xlim([0, 1])
-        plt.ylim([0, 1])
-        plt.ylabel('True Positive Rate')
-        plt.xlabel('False Positive Rate')
-        plt.show()
+        Predictor._show_ROC_curve(self.blind_y_ture, self.blind_y_pred)
 
 
 class LGBMPredictor(Predictor):
-    def load_model(self, file_name):
-        pass
 
-    def output_metrix(self):
-        pass
-
-    def feature_importance(self):
-        pass
-
-    def predict(self, target_file):
-        pass
-
-    def train(self):
-        pass
+    def __init__(self, kind, X, y, **kwargs):
+        self._kind = kind  # animal or plant
+        self._X = X
+        self._y = y
+        self.params: dict = kwargs  # learner's params
+        self._k_fold = StratifiedKFold(n_splits=5, random_state=42, shuffle=True)  # Cross validation
+        self.lgb_clf = LGBMClassifier(**self.params)  # Build model
+        self.blind_y_ture = None
+        self.blind_y_pred = None
 
     def search_params(self, params_dict):
-        pass
+        """
 
-    def save_model(self, name, path):
-        pass
+        @param params_dict:
+        @return: best score and best params
+        """
+        adapter = GridSearchCV(estimator=self.lgb_clf,
+                               scoring='roc_auc',
+                               param_grid=params_dict,
+                               cv=5,
+                               verbose=10)  # using grid search to find the best params
+        adapter.fit(self._X, self._y)
+        self.lgb_clf = adapter.best_estimator_  # Update the model=======THE MOST IMPORTANT CODE
+        return adapter.best_score_, adapter.best_params_
+
+    def train(self):
+        for train_idx, test_idx in self._k_fold.split(self._X, self._y):
+            X_train, y_train = self._X[train_idx, :], self._y[train_idx]
+            X_test, y_test = self._X[test_idx, :], self._y[test_idx]
+
+            self.lgb_clf.fit(X_train, y_train)
+            print('Accuracy:', self.lgb_clf.score(X_test, y_test))  # Show the accuracy of each epoch
+
+    def save_model(self, path):
+        try:
+            file_name = os.path.join(path, 'lgbm_' + self._kind + '.pickle')
+            if os.path.exists(file_name):
+                os.remove(file_name)
+            pickle.dump(self.lgb_clf, open(file_name, 'wb'))
+            print('MODEL SAVED.')
+        except FileExistsError as fee:
+            print('File exists ERROR!', fee)
+        except FileNotFoundError as fnfe:
+            print('File not found ERROR!', fnfe)
+
+    def load_model(self, file_name):
+        self.lgb_clf = pickle.load(open(file_name, 'rb'))
+
+    def output_metrix(self):
+        Predictor._report(self.blind_y_ture, self.blind_y_pred)
+
+    def feature_importance(self):
+        """
+        Draw the plot of feature importance
+        @return:
+        """
+        return lgb.plot_importance(self.lgb_clf)
+
+    def predict(self, target_file):
+        dataset_obj = None
+        if self._kind == 'plant':
+            dataset_obj = PlantDataSet(target_file)
+        elif self._kind == 'animal':
+            dataset_obj = AnimalDataSet(target_file)
+        else:
+            raise Exception("No this item.")
+        dataset_obj.data_clean()
+        dataset_obj.normalize('l2')
+        self.blind_y_ture = dataset_obj.get_label()
+        self.blind_y_pred = self.lgb_clf.predict(dataset_obj.get_data())
+
+    def show_ROC_curve(self):
+        Predictor._show_ROC_curve(self.blind_y_ture, self.blind_y_pred)
