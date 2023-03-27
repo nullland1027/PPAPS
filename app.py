317fbad04408d491a787c446706ad0894243b495
Zhanghaohan <nullland1027@outlook.com>
2023-03-27 09:57:42 +0800

    update pages and css

--- app.py
+++ app.py
@@ -10,45 +10,77 @@ from flask import redirect, url_for
 app = Flask(__name__)  # 申明app对象
 redis = Redis(host='127.0.0.1', port=6379)  # host:数据库的服务名
 
+# Global variables area
+IS_LOGIN = False  # 用户是否登陆
+
+
+@app.after_request
+def set_response_headers(response):
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+    return response
+
 
 @app.route('/')
-@app.route('/home')
 def index():
+    """The website cover page"""
     return render_template('index.html')
 
 
-@app.route('/login')
+@app.route('/check-state')
+def check_if_login():
+    """Check"""
+    if IS_LOGIN:
+        return redirect(url_for("home"))
+    else:
+        return redirect(url_for("login"))
+
+
+@app.route('/login', methods=['POST', 'GET'])
 def login():
-    return render_template('login.html')
+    global IS_LOGIN
+    if IS_LOGIN:
+        return redirect(url_for('home'))
+    if request.method == 'POST':  # received user's input
+        user_id = request.form['email']
+        pwd = request.form['password']
+        if user_id == 'admin@super.com' and pwd == '123':
+            IS_LOGIN = True
+            return redirect(url_for("home"))
+    else:  # not receive user's input 由其他页面跳转而来
+        return render_template('login.html')
 
 
-@app.route("/hide")
-def admin():
-    return redirect(url_for("login"))  # 回到登陆页面
+@app.route('/signup', methods=['POST', 'GET'])
+def sign_up():
+    return render_template('sign-up.html')
 
 
-@app.route('/redis')
-def hello():
-    redis.incr('hits')
-    count = redis.get('hits').decode('utf-8')
-    return f'Hello! I have been seen {count} times.'
+@app.route('/home-page', methods=['POST', 'GET'])
+def home():
+    """After click"""
+    global IS_LOGIN
+    if not IS_LOGIN:  # 没有登陆
+        return redirect(url_for("login"))
 
-
-@app.route('/', methods=['POST', 'GET'])
-def upload():
     kind = request.form.get('kind')
     al = request.form.get('al')
     file = request.files.get('file')
+
     if not file:
-        return render_template('index.html', message='No file selected!')
+        return render_template('home-page.html')  # , message='No file selected!')
     filename = file.filename
     # file.save(os.path.join("upload", filename))
-    return redirect(url_for('success'))
+    return redirect(url_for('file_checking', kind=kind, al=al))
 
 
-@app.route('/success')
-def success():
-    return 'File uploaded successfully.'
+@app.route('/file-checking')
+def file_checking():
+    """The file has been uploaded"""
+    kind = request.args.get('kind')
+    al = request.args.get('al')
+    return f'File uploaded successfully Ready to infer. You selected: {kind, al}'
 
 
 if __name__ == '__main__':
---
+++ static/script.js
@@ -0,0 +1,56 @@
+function displayFileName() {
+    const fileInput = document.getElementById('file');
+    const fileName = fileInput.files[0].name;
+    const fileNameDisplay = document.getElementById('fileName');
+    fileNameDisplay.innerHTML = `File selected：${fileName}`;
+}
+
+function checkFileSelected() {
+    const fileInput = document.querySelector('input[type=file]');
+    if (!fileInput.value) {
+        alert('Please select a file to upload.');
+        return false;
+    }
+    return true;
+}
+
+function validateForm() {
+    let i;
+    const radio_kind = document.getElementsByName("kind");
+    const radio_al = document.getElementsByName("al");
+    let formValid_kind = false;
+    let formValid_al = false;
+    for (i = 0; i < radio_kind.length; i++) {
+        if (radio_kind[i].checked) {
+            formValid_kind = true;
+            break;
+        }
+    }
+    for (i = 0; i < radio_al.length; i++) {
+        if (radio_al[i].checked) {
+            formValid_al = true;
+            break;
+        }
+    }
+
+    if (!formValid_kind || !formValid_al) {
+        alert("Please select an option!");
+        return false;
+    }
+    return true;
+}
+
+function test() {
+    // 获取按钮元素
+    const myButton = document.getElementById('myButton');
+
+    // 监听按钮点击事件
+    myButton.addEventListener('click', function() {
+        // 使用 AJAX 或 Fetch API 发送请求到 Flask 后台
+        fetch('/my-endpoint')
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.error(error));
+    });
+}
+
